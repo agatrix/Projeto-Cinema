a906ac820b419d6063e63c0668d7c6405b2ae726
HeullerRamos <heullervieira4@gmail.com>
2024-07-20 18:54:51 -0300

    Leitura do arquivo Filmes

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Filme {
+public:
+  string idFilme;
+  string tipoDoFilme;
+  string tituloPrimario;
+  string tituloOriginal;
+  bool isAdult;
+  int anoLancamento;
+  int anoTermino;
+  int duracao;
+  string genero;
+Filme(const string& idFilme, const string& tipoDoFilme, const string& tituloPrimario, const string& tituloOriginal,
+  bool isAdult, int anoLancamento, int anoTermino, int duracao, const string& genero)
+: idFilme(idFilme), tipoDoFilme(tipoDoFilme), tituloPrimario(tituloPrimario), tituloOriginal(tituloOriginal),
+  isAdult(isAdult), anoLancamento(anoLancamento), anoTermino(anoTermino), duracao(duracao), genero(genero) {}
+};
+
+class Cinema {
+public:
+  string id;
+  string nomeDoCinema;
+  double cordX;
+  double cordy;
+  float preco;
+  list<Filme> filmes;
+};
+int main() {
+  // ofstream myStream;
+  ifstream arquivo("filmesCrop.txt");
+  vector<Filme> filmes;
+  string linha;
+  if (arquivo.is_open()) {
+    getline(arquivo, linha);
+
+    while (getline(arquivo, linha)) {
+      istringstream ss(linha);
+      string ids, tipo, tituloPrimario, tituloOriginal, isAdultStr, anoLancamentoStr,anoTerminoStr,duracaoStr,genero;
+
+      getline(ss, ids, '\t');
+      getline(ss, tipo, '\t');
+      getline(ss, tituloPrimario, '\t');
+      getline(ss, tituloOriginal, '\t');
+      getline(ss, isAdultStr, '\t');
+      getline(ss, anoLancamentoStr, '\t');
+      getline(ss, anoTerminoStr, '\t');
+      getline(ss, duracaoStr, '\t');
+      getline(ss, genero, '\t');
+      
+      try {
+        bool isAdult = (isAdultStr != "\\N" && isAdultStr == "1");
+        int anoLancamento = (anoLancamentoStr != "\\N") ? stoi(anoLancamentoStr) : 0;
+        int anoTermino = (anoTerminoStr != "\\N" && !anoTerminoStr.empty()) ? stoi(anoTerminoStr) : 0;
+        int duracao = (duracaoStr != "\\N") ? stoi(duracaoStr) : 0;
+        if(genero =="\\N"){
+          genero = "NULL";
+        }
+        Filme filme(ids, tipo, tituloPrimario, tituloOriginal, isAdult, anoLancamento, anoTermino, duracao, genero);
+              
+        filmes.push_back(filme);
+      } catch (const invalid_argument& e) {
+          cerr << "Erro de conversão: argumento inválido para ano de lançamento, ano de término ou duração" << endl;
+      } catch (const out_of_range& e) {
+          cerr << "Erro de conversão: valor fora do intervalo para ano de lançamento, ano de término ou duração" << endl;
+      }
+    }
+    arquivo.close();
+  }else {
+      cerr << "Não foi possível abrir o arquivo." << endl;
+      return 1;
+  }
+  for (const auto& filme : filmes) {
+    cout << "ID do Filme: " << filme.idFilme
+         << ", Tipo do Filme: " << filme.tipoDoFilme
+         << ", Título Primário: " << filme.tituloPrimario
+         << ", Título Original: " << filme.tituloOriginal
+         << ", Adulto: " << (filme.isAdult ? "Sim" : "Não")
+         << ", Ano de Lançamento: " << filme.anoLancamento
+         << ", Ano de Término: " << filme.anoTermino
+         << ", Duração: " << filme.duracao
+         << ", Gênero: " << filme.genero << endl;
+  }
+
+  return 0;
+}
